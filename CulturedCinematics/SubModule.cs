88847cb907b0cc631b7af0728e62a7e4b17c368d
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-11-28 17:12:01 +0100

    Initial commit

---
+++ CulturedCinematics/SubModule.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace CulturedCinematics
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony("CulturedCinematics.patches").PatchAll();
+        }
+    }
+}
---
+++ CulturedCinematics/patches/Marriage/MarriageSceneCulturePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Helpers;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SceneInformationPopupTypes;
+
+namespace FullScreenCinematics.Patches.Marriage
+{
+    [HarmonyPatch(typeof(MarriageSceneNotificationItem), nameof(MarriageSceneNotificationItem.SceneID), MethodType.Getter)]
+    internal class MarriageSceneCulturePatch
+    {
+        [HarmonyPostfix]
+        static void Postfix(ref MarriageSceneNotificationItem __instance, ref string __result)
+        {
+            string text = string.Concat(new object[] { "scn_cutscene_wedding", "_" , (SettlementHelper.FindNearestSettlementToPoint(__instance.GroomHero.GetCampaignPosition())).Culture.StringId });
+            var trySceneExist = new FallbackForSceneMissing();
+            text = trySceneExist.TryGetSceneExist(text) ? text : "scn_cutscene_wedding";
+            __result = text;
+        }
+    }
+}
